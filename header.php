8588eacae5d0a0115324f201344e675a665a7f9e
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-06-18 21:14:36 +0700

    add header no style

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
      <meta name="author" content="Wolfactive - HuyNguyen - PhuongNam">
-  	<link rel="profile" href="https://wolfactive.net/">
+  	 <link rel="profile" href="https://wolfactive.net/">
      <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
      <link rel="preload" href="<?php echo get_theme_file_uri('dist/css/webfonts/fa-brands-400.woff2') ?>" as="font" type="font/woff2" crossorigin>
      <link rel="preload" href="<?php echo get_theme_file_uri('dist/css/webfonts/fa-regular-400.woff2') ?>" as="font" type="font/woff2" crossorigin>
@@ -25,28 +25,37 @@
              $custom_logo_id = get_theme_mod( 'custom_logo' );
              $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
                 ?>
-  	        <img src="<?php echo $image[0]; ?>" alt="logo-nha-pho-sai-gon">
+  	        <img src="<?php echo $image[0]; ?>" alt="logo-gamewiki">
   	      </a>
+          <span class="text--light">
+            The largest in Japan! Game strategy information media
+          </span>
   	   </div>
-  	   <div class="header__item dp--none">
-  	      <?php
-  	       wp_nav_menu(array(
-  		    'theme_location' => 'headerMenuLocation' ));
-  	      ?>
-  	   </div>
-  	   <div class="header__item d--none dp--block">
-  	      <button class="btn text--light" id="navBtn" aria-label="btn-navbar">
-  	          <i class="fas fa-bars icon--text"></i>
-  	      </button>
-  	   </div>
+       <div class="header__item">
+         <form class="header__form-item" action="<?php bloginfo('url')?>" method="get">
+           <div class="header__search">
+              <label for="inputHeaderSearch"><i class="fas fa-search" class="icon--text"></i></label>
+              <input class="input" type="search" id="inputHeaderSearch" name="s" value="" placeholder="Tìm kiếm">
+              <input type="hidden" value="1" name="sentence" />
+              <input type="hidden" value="post" name="post_type" />
+          </div>
+          <div class="header__search">
+            <button type="submit" aria-label="search-header-button" name="button-search">Search</button>
+          </div>
+         </form>
+       </div>
+       <div class="header__item">
+         <div class="header__item-social">
+           <a href="#" target="_blank">
+             <i class="fab fa-facebook-square icon"></i>
+           </a>
+         </div>
+         <div class="header__item-social">
+           <a  href="#" target="_blank">
+             <i class="fab fa-twitter icon"></i>
+           </a>
+         </div>
+       </div>
   	</div>
-  </div>
-    <div class="main--background" id="headerNavBar">
-     <div class="container navbar__mb">
-       <?php
-       wp_nav_menu(array(
-      'theme_location' => 'headerMenuLocation' ));
-      ?>
-    </div>
   </div>
 </section>
