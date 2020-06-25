a3b00cd07a42db85d9b706d3b3d70ca96ece1ab5
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-06-25 20:16:14 +0700

    merge

--- sections/menu-ranking.php
+++ sections/menu-ranking.php
@@ -2,7 +2,7 @@
   <div class="menuRanking__contain">
     <div class="menuRanking__banner">
       <a href="<?php the_field('link_ads_banner_right_bar') ?>" target="_blank" rel="noreferrer">
-        <img src="<?php the_field('ads_banner_right_bar') ?>" alt="ads-banner">
+        <img src="<?php echo hk_get_image(get_field('ads_banner_right_bar'),266,150)  ?>" alt="ads-banner">
       </a>
     </div>
     <div class="menuRanking__content">
