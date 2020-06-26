870b4da047720552a6262c879932a97aa4df0dea
huyminh nguyenpham <huyminhnguyenpham@gmail.com>
2020-06-27 00:33:45 +0700

    add responsive

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
      <meta charset="<?php bloginfo('charset'); ?>" />
-     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0" /> -->
      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
      <meta name="author" content="Wolfactive - HuyNguyen - PhuongNam">
   	 <link rel="profile" href="https://wolfactive.net/">
